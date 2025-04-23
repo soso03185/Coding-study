fc621e286ba2277da1c44ccf6a27049e360ec81e
Kim JaeHyeon <34054994+soso03185@users.noreply.github.com>
2025-04-23 20:39:16 +0900

    [level 0] Title: 배열 비교하기, Time: 0.84 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        
+        if(arr1.Length == arr2.Length)
+        {
+            if(arr1.Sum() == arr2.Sum())
+                return 0;                
+        
+            return arr1.Sum() > arr2.Sum() ? 1 : -1;
+        }
+        
+        return arr1.Length > arr2.Length ? 1 : -1;
+    }
+}
